da889f6f5c22e2b4f9963535d0689141ca0902b8
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-03-19 14:24:27 +0200

    Use react 16.13.0; added react to optimizePackages; fix plugin loading by razzle.config

--- razzle.config.js
+++ razzle.config.js
@@ -1,18 +1,10 @@
 const path = require('path');
 
-// const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
-// const autoprefixer = require('autoprefixer');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
-// const projectRootPath = path.resolve('.');
-// const voltoConfig = require(`${voltoPath}/razzle.config`);
-// const voltoPath = base.resolveVoltoPath('.');
-
 const base = require('./src/develop/volto-base/src').razzle;
-
 const config = base.BaseConfig(path.resolve('.'));
+const razzleModify = config.modify;
 
 module.exports = {
-  plugins: base.plugins,
-  modify: config.modify, //razzleModify(voltoPath),
+  plugins: base.defaultPlugins,
+  modify: razzleModify,
 };
